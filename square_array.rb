1d2dae8f55ec9bcd4390f3b012d1a4e119bfa718
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-13 06:27:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,12 @@
 def square_array(array)
   new_array = []
+<<<<<<< HEAD
   array.each do |a|
     new_array << (a*a)
   end
     return new_array
+=======
+  array.each{|a| new_array.push(a*a)}
+  return new_array
+>>>>>>> aaa038232e5a8d8e71b4d2e8bccf595bf5252b06
 end
